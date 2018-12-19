c3fd2e81688981cc2f4d47e9ecf0164d9282764c
DanStach <Stachd@gmail.com>
2018-12-19 06:43:22 -0600

    minor cleanup

--- test/JG.Demo.CoreTests/PageObjects/WWEXHomePage.cs
+++ test/JG.Demo.CoreTests/PageObjects/WWEXHomePage.cs
@@ -7,13 +7,21 @@ namespace JG.Demo.CoreTests.PageObjects
 {
     public class WWEXHomePage : DemoPage
     {
+        private string logoXPath= "//*[@id='logo']";
+        private string menuCareersXPath = "//*[@id='menu-main-1']/li[4]";
+
         public WWEXHomePage(IWebDriver driver, Uri baseUrl, string path) : base(driver, baseUrl, path)
         {
         }
 
         public IWebElement logo
         {
-            get { return this.Driver.FindElement(By.XPath(@"//*[@id='logo']")); }
+            get { return this.Driver.FindElement(By.XPath(logoXPath)); }
+        }
+
+        public IWebElement menuCareers
+        {
+            get { return this.Driver.FindElement(By.XPath(menuCareersXPath)); }
         }
     }
 }
